f8658a3f6ab5c1bf40d3f95dcafb3ae5499ddb8e
saesang <takseha98@gmail.com>
2025-02-18 10:18:15 +0900

    feat(#3): ErrorHandlingActivity 구현

--- data/src/main/java/com/example/data/retrofit/ServerApi.kt
+++ data/src/main/java/com/example/data/retrofit/ServerApi.kt
@@ -4,11 +4,12 @@ import com.example.data.dto.FortuneInfoDto
 import com.example.data.dto.UserInfoDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ServerApi {
     @GET("/api/fortune")
-    suspend fun fetchFortuneInfo(username: String): Response<FortuneInfoDto>
+    suspend fun fetchFortuneInfo(@Query("username") username: String): Response<FortuneInfoDto>
 
     @GET("/api/userInfo")
-    suspend fun fetchUserInfo(username: String): Response<UserInfoDto>
+    suspend fun fetchUserInfo(@Query("username") username: String): Response<UserInfoDto>
 }
