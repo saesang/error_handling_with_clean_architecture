723acc2b43a52fa4dfe83124c9a68a4957b43215
saesang <takseha98@gmail.com>
2025-02-19 16:26:28 +0900

    feat(#7): domain 모듈 ErrorHandling 로직 구현

--- domain/src/main/java/com/example/domain/usecase/FetchTotalInfoUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/FetchTotalInfoUseCase.kt
@@ -1,13 +1,19 @@
 package com.example.domain.usecase
 
+import com.example.domain.model.ResultWrapper
 import com.example.domain.model.TotalInfoData
 import com.example.domain.repository.TotalInfoRepository
+import com.example.domain.usecase.validator.UsernameValidator
 
 class FetchTotalInfoUseCase(
     private val totalInfoRepository: TotalInfoRepository
 ) {
-    suspend operator fun invoke(username: String): TotalInfoData {
+    suspend operator fun invoke(username: String): ResultWrapper<TotalInfoData> {
         // 서버 요청
+        // validation 체크
+        val validationResult = UsernameValidator.validate<TotalInfoData>(username)
+        if (validationResult is ResultWrapper.Error) return validationResult
+
         return totalInfoRepository.fetchTotalInfo(username)
     }
 }
--- domain/src/main/java/com/example/domain/usecase/GetTotalInfoUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetTotalInfoUseCase.kt
@@ -1,13 +1,20 @@
 package com.example.domain.usecase
 
+import com.example.domain.model.Failure
+import com.example.domain.model.ResultWrapper
 import com.example.domain.model.TotalInfoData
 import com.example.domain.repository.TotalInfoRepository
+import com.example.domain.usecase.validator.UsernameValidator
 
 class GetTotalInfoUseCase(
     private val totalInfoRepository: TotalInfoRepository
 ) {
-    suspend operator fun invoke(username: String): TotalInfoData? {
+    suspend operator fun invoke(username: String): ResultWrapper<TotalInfoData?> {
         // DB 요청, DB에 데이터 없으면 null 반환
+        // validation 체크
+        val validationResult = UsernameValidator.validate<TotalInfoData?>(username)
+        if (validationResult is ResultWrapper.Error) return validationResult
+
         return totalInfoRepository.getTotalInfo(username)
     }
 }
