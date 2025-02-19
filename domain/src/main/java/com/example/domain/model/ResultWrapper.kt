5815f765a6f734bf2228218dae46c3892346eaf2
saesang <takseha98@gmail.com>
2025-02-19 11:16:22 +0900

    feat(#5): domain 모듈 error 관련 객체 구현

---
+++ domain/src/main/java/com/example/domain/model/ResultWrapper.kt
@@ -0,0 +1,6 @@
+package com.example.domain.model
+
+sealed class ResultWrapper<out T>(open val data: T?) {
+    data class Success<T>(override val data: T) : ResultWrapper<T>(data)
+    data class Error<T>(val error: Failure?, override val data: T? = null) : ResultWrapper<T>(data)
+}
