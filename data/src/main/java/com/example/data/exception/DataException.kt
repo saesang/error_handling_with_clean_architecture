b7baacdd250696b9d360344a2a28e1a16905ecc8
saesang <takseha98@gmail.com>
2025-02-25 13:53:23 +0900

    feat(#9): DataException 수정

--- data/src/main/java/com/example/data/exception/DataException.kt
+++ data/src/main/java/com/example/data/exception/DataException.kt
@@ -3,12 +3,13 @@ package com.example.data.exception
 import android.database.sqlite.SQLiteConstraintException
 import android.database.sqlite.SQLiteException
 import com.example.domain.model.Failure
+import com.google.gson.JsonParseException
+import com.google.gson.JsonSyntaxException
 import retrofit2.HttpException
-import java.io.FileNotFoundException
-import java.io.IOException
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
+import java.util.concurrent.TimeoutException
 
 
 sealed class DataException(message: String, cause: Throwable? = null) : Exception(message, cause) {
@@ -16,18 +17,20 @@ sealed class DataException(message: String, cause: Throwable? = null) : Exceptio
     class ServerError(message: String, cause: Throwable? = null) : DataException(message, cause)
     class ClientError(message: String, cause: Throwable? = null) : DataException(message, cause)
     class ConnectionError(message: String, cause: Throwable? = null) : DataException(message, cause)
-    class TimeoutError(message: String, cause: Throwable? = null) : DataException(message, cause)
+    class ParseError(message: String, cause: Throwable? = null) : DataException(message, cause)
     class DatabaseError(message: String, cause: Throwable? = null) : DataException(message, cause)
-    class FileIOError(message: String, cause: Throwable? = null) : DataException(message, cause)
-    class MemoryError(message: String, cause: Throwable? = null) : DataException(message, cause)
-    class UnknownError(message: String, cause: Throwable? = null) : DataException(message, cause)
+    class TimeoutError(message: String, cause: Throwable? = null) : DataException(message, cause)
     class EmptyDataError(message: String, cause: Throwable? = null) : DataException(message, cause)
+    class UnknownUserError(message: String, cause: Throwable? = null) : DataException(message, cause)
+    class InvalidMeasurementError(message: String, cause: Throwable? = null) : DataException(message, cause)
+    class InvalidInputError(message: String, cause: Throwable? = null) : DataException(message, cause)
     class MappingError(message: String, cause: Throwable? = null) : DataException(message, cause)
+    class UnknownError(message: String, cause: Throwable? = null) : DataException(message, cause)
 
     companion object {
         fun mapToDataException(e: Throwable): DataException {
             return when (e) {
-                is ConnectException ->
+                is ConnectException, is SocketTimeoutException ->
                     NetworkConnectionError("서버 연결 실패", e)
 
                 is HttpException -> {
@@ -44,17 +47,14 @@ sealed class DataException(message: String, cause: Throwable? = null) : Exceptio
                 is UnknownHostException ->
                     ConnectionError("인터넷 연결 없음", e)
 
-                is SocketTimeoutException ->
-                    TimeoutError("서버 연결 시간 초과", e)
+                is JsonSyntaxException, is JsonParseException ->
+                    ParseError("데이터 파싱 오류", e)
 
                 is SQLiteConstraintException, is SQLiteException ->
                     DatabaseError("데이터베이스 오류", e)
 
-                is FileNotFoundException, is IOException ->
-                    FileIOError("파일 I/O 오류", e)
-
-                is OutOfMemoryError ->
-                    MemoryError("메모리 부족", e)
+                is TimeoutException ->
+                    TimeoutError("응답 시간 초과", e)
 
                 else -> UnknownError("기타 오류", e)
             }
@@ -64,10 +64,11 @@ sealed class DataException(message: String, cause: Throwable? = null) : Exceptio
             return when(this) {
                 is NetworkConnectionError, is ServerError, is ClientError -> Failure.ServerFailure(this)
                 is ConnectionError, is TimeoutError -> Failure.NetworkFailure(this)
-                is DatabaseError -> Failure.DatabaseFailure(this)
-                is FileIOError -> Failure.FileIOFailure(this)
-                is EmptyDataError, is MappingError -> Failure.BusinessFailure(this)
-                is MemoryError, is UnknownError -> Failure.UnknownFailure(this)
+                is DatabaseError, is ParseError -> Failure.DatabaseFailure(this)
+                is InvalidInputError -> Failure.ValidationFailure("입력값 오류", this)
+                is InvalidMeasurementError -> Failure.ValidationFailure("측정값 오류", this)
+                is UnknownUserError, is EmptyDataError, is MappingError -> Failure.BusinessFailure(this)
+                is UnknownError -> Failure.UnknownFailure(this)
             }
         }
     }
